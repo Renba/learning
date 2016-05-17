5469ee96907a01cc114aaa6405c9fa9a5dd64433
rafael ortiz <dario.ortiz@correo.uady.mx>
2016-05-17 14:38:23 -0500

    Se sube BD actualizada

--- learning.sql
+++ learning.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 17-05-2016 a las 10:45:24
--- Versión del servidor: 10.1.9-MariaDB
--- Versión de PHP: 5.5.30
+-- Tiempo de generación: 17-05-2016 a las 21:37:53
+-- Versión del servidor: 10.1.13-MariaDB
+-- Versión de PHP: 5.6.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -41,15 +41,18 @@ CREATE TABLE `grades` (
 
 CREATE TABLE `students` (
   `id` int(10) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(20) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `students`
 --
 
-INSERT INTO `students` (`id`, `name`) VALUES
-(1, 'Abner Collí');
+INSERT INTO `students` (`id`, `name`, `username`, `password`) VALUES
+(1, 'prueba', 'prueba', '123456'),
+(2, 'prueba2', 'prueba2', '12345678');
 
 -- --------------------------------------------------------
 
@@ -62,29 +65,6 @@ CREATE TABLE `subjects` (
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `codename` varchar(120) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
---
--- Volcado de datos para la tabla `subjects`
---
-
-INSERT INTO `subjects` (`id`, `name`, `codename`) VALUES
-(1, 'Algebra Superior 1', 'alg_sup1'),
-(2, 'Calculo 1', 'calc1'),
-(3, 'Computacion 1', 'comp1'),
-(4, 'Geometria Analitica', 'geo_ana'),
-(5, 'Algebra Superior 2', 'alg_sup2'),
-(6, 'Calculo 2', 'calc2'),
-(7, 'Computacion 2', 'comp2'),
-(8, 'Geometria Moderna', 'geo_mod'),
-(9, 'Algebra Lineal I', 'alg_lin1'),
-(10, 'Calculo III', 'calc3'),
-(11, 'Analisis Numerico I', 'ana_num1'),
-(12, 'Ecs. Diferenciales I', 'ecs_dif1'),
-(13, 'Probabilidad', 'proba'),
-(14, 'Algebra Lineal II', 'alg_lin2'),
-(15, 'Analisis Numerico II', 'ana_num2'),
-(16, 'Ecs. Diferenciales II', 'ecs_dif2'),
-(17, 'Inf. Estadastica', 'inf_est');
 
 --
 -- Índices para tablas volcadas
@@ -123,12 +103,12 @@ ALTER TABLE `grades`
 -- AUTO_INCREMENT de la tabla `students`
 --
 ALTER TABLE `students`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT de la tabla `subjects`
 --
 ALTER TABLE `subjects`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
